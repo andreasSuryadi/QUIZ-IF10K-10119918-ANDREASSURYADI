b492fbd059a0d57ee64f136f12b481e7dc1eea28
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-11-27 20:31:26 +0700

    First commit untuk quiz

---
+++ src/quizif10k10119918andreassuryadi/QUIZIF10K10119918ANDREASSURYADI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizif10k10119918andreassuryadi;
+
+/**
+ *
+ * @author andreas
+ */
+public class QUIZIF10K10119918ANDREASSURYADI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/quizif10k10119918andreassuryadi/ServicePrice.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizif10k10119918andreassuryadi;
+
+/**
+ *
+ * @author andreas
+ */
+public class ServicePrice {
+    
+}
